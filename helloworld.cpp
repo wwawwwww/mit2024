0bae09d2092a7901c969a55007c456b01da85a60
Artem Drozhdev <zappak21@mail.ru>
2024-04-10 11:17:14 +0300

    fix: now programm works correctly upd: now fib nums starts with 0 1 instead of 1 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -3,17 +3,18 @@
 using namespace std;
 int fib(int n){
 	if (n == 1 || n == 2){
-		return 1;
+		if (n == 1)
+			return 0;
+		else return 1;
 	}
-	if (n > 2){
-		return fib(n - 1) +fib(n - 2);
-	}
+	else return fib(n - 1) + fib(n - 2);
 }
+
 int main(){
 	cout << "Hello world" << endl;
 	int n;
 	cout << "Enter N of Fib num: ";
-	cin >> n	 
+	cin >> n;	 
 	cout << endl << fib(n) << endl;
 	return 0;
 }
