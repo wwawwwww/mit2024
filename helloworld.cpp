e32c5c8eacf68377be7bc78a0cb0f42dbbec4f63
Artem Drozhdev <zappak21@mail.ru>
2024-04-10 11:04:05 +0300

    upd: new function - fib. that func can calculate fibonacci numbers

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
 using namespace std;
-
+int fib(int n){
+	if (n == 1 || n == 2){
+		return 1;
+	}
+	if (n > 2){
+		return fib(n - 1) +fib(n - 2);
+	}
+}
 int main(){
 	cout << "Hello world" << endl;
+	int n;
+	cout << "Enter N of Fib num: ";
+	cin >> n	 
+	cout << endl << fib(n) << endl;
 	return 0;
 }
